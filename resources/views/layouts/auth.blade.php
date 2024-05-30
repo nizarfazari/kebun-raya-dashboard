12ab424024a2d07fea9039154bfefad27f0d8c6c
Nizar Fazari <nexsel32@gmail.com>
2024-05-30 12:10:29 +0700

    test asset_secure

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,14 +7,14 @@
     <title>@yield('title') {{ config('app.name') }}</title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="{{ asset('/assets/modules/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/modules/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/modules/fontawesome/css/all.min.css') }}">
 
     <!-- CSS Libraries -->
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/css/components.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/css/components.css') }}">
 
     @stack('page-styles')
     <!-- Start GA -->
@@ -38,21 +38,21 @@
     </div>
     @stack('before-script')
     <!-- General JS Scripts -->
-    <script src="{{ asset('/assets/modules/jquery.min.js') }}"></script>
-    <script src="{{ asset('/assets/modules/popper.js') }}"></script>
-    <script src="{{ asset('/assets/modules/tooltip.js') }}"></script>
-    <script src="{{ asset('/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-    <script src="{{ asset('/assets/modules/moment.min.js') }}"></script>
-    <script src="{{ asset('/assets/js/stisla.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/popper.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/tooltip.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/modules/moment.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/stisla.js') }}"></script>
 
     <!-- JS Libraies -->
 
     <!-- Page Specific JS File -->
 
     <!-- Template JS File -->
-    <script src="{{ asset('/assets/js/scripts.js') }}"></script>
-    <script src="{{ asset('/assets/js/custom.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/scripts.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/custom.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
